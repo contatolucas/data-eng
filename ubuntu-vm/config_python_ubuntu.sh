89695903fc86fd44b9acdd6dcfd5cbc15200ab36
Lucas Vieira <contatolucas@gmail.com>
2021-01-22 12:05:05 -0300

    Atualiza config_python_ubuntu.sh

--- ubuntu-vm/config_python_ubuntu.sh
+++ ubuntu-vm/config_python_ubuntu.sh
@@ -16,11 +16,12 @@ wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | sudo apt-key add -
 echo "deb https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
 #wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 #echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y python3-pip sqlite3 sqlitebrowser \
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y python3-pip python3-venv sqlite3 sqlitebrowser \
 	 libpq-dev python-dev apt-transport-https ca-certificates curl gnupg-agent software-properties-common \
 	 git git-gui git-doc default-jre default-jdk dbeaver-ce google-chrome-stable
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get update && sudo apt-get install -y nodejs
+sudo npm install -g aws-cdk
 echo -e "\n\n# JDK" >> ~/.bashrc
 echo -e "export JAVA_HOME=/usr/bin" >> ~/.bashrc
 echo -e "export PATH=\$JAVA_HOME:\$PATH" >> ~/.bashrc
